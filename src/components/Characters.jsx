5dee48dd1bae9f2bc08daa4fa5e029b0bbfb2450
leninner <mazabandalenin180@gmail.com>
2022-03-22 15:49:48 -0500

    Update

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -34,9 +34,11 @@ export const Characters = () => {
     <>
       <div className='grid w-full grid-cols-1 lg:container md:grid-cols-2 lg:grid-cols-3 place-items-center'>
         {loading && <p className='text-3xl text-white'>Loading...</p>}
+
         {charactersToShow?.map((value) => {
           return <CharacterCard {...value} />;
         })}
+
         {error && <p className='text-3xl text-white'>Error...</p>}
       </div>
       <Pagination onPreviousPage={onPreviousPage} onNextPage={onNextPage} />
