6f0a5657112ee3ef53bcc30cb401d73bc8a88893
leninner <mazabandalenin180@gmail.com>
2022-03-22 10:40:34 -0500

    Update UI

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -2,7 +2,7 @@ import { useFetchInitialData } from '../Hooks/useFetchInitialData';
 import { CharacterCard } from './CharacterCard';
 
 export const Characters = () => {
-  const { data, loading, error } = useFetchInitialData({ limit: 20, offset: 660 });
+  const { data, loading, error } = useFetchInitialData({ limit: 20, offset: 500 });
 
   return (
     <div className='grid w-full grid-cols-1 lg:container md:grid-cols-2 lg:grid-cols-3 place-items-center'>
